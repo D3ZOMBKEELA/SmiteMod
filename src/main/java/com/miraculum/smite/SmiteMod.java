d4919061eb98ad2c126325c3f378a46d160710ee
Tylar Cunningham <T10Cunningham@gmail.com>
2015-09-02 21:00:33 -0400

    Initial mod commit

---
+++ src/main/java/com/miraculum/smite/SmiteMod.java
@@ -0,0 +1,33 @@
+package com.miraculum.smite;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "smitemod", name = "Smite Mod", version = "0.0a")
+public class SmiteMod
+{
+	@Instance("smitemod")
+	public static SmiteMod instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
